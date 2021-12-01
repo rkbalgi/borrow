824df8ac72591a8e9d60fa9b7ad7c2723d30f76f
Raghavendra Balgi <rkbalgi@gmail.com>
2021-12-01 19:32:53 +0530

    feat(security): auth setup complete

--- src/main/kotlin/com/github/rkbalgi/apps/borrow/security/Crypto.kt
+++ src/main/kotlin/com/github/rkbalgi/apps/borrow/security/Crypto.kt
@@ -5,10 +5,10 @@ import org.bouncycastle.crypto.digests.SHA256Digest
 
 
 fun sha256(data: String): String {
-    SHA256Digest().apply {
+    SHA256Digest().run {
 
         val res = ByteArray(this.digestSize)
-        update(data.toByteArray(Charsets.UTF_8),0,data.length)
+        update(data.toByteArray(Charsets.UTF_8), 0, data.length)
         doFinal(res, 0)
         return Hex.encodeHexString(res)
     }
