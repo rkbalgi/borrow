641533cba901faf628cfd34e92be83a69e0fd2da
Raghavendra Balgi <rkbalgi@gmail.com>
2021-12-09 00:00:03 +0530

    feat(add-asset): tidy up

--- src/test/kotlin/com/github/rkbalgi/apps/borrow/data/JpaTests.kt
+++ src/test/kotlin/com/github/rkbalgi/apps/borrow/data/JpaTests.kt
@@ -1,6 +1,7 @@
 package com.github.rkbalgi.apps.borrow.data
 
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,6 +14,7 @@ class JpaTests(@Autowired val assetRepo: AssetRepo) {
 
 
     @Test
+    @Disabled
     fun test_assets() {
         val assets = mutableListOf<Asset>()
         assetRepo.findAll().toCollection(assets)
